2dde713239fb4bcd14100d17d2928006b26c149d
Koziakov Hlib <hlibs.work@gmail.com>
2024-11-20 00:59:12 +0200

    implemented 3 functions

--- app/main.py
+++ app/main.py
@@ -12,9 +12,11 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "errors": [format_linter_error(error) for error in errors],
         "path": file_path,
-        "status": "failed" if [format_linter_error(error) for error in errors] else "passed"
+        "status": "failed" if [format_linter_error(error)
+                               for error in errors] else "passed"
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(key, value) for key, value in linter_report.items()]
+    return [format_single_linter_file(key, value)
+            for key, value in linter_report.items()]
